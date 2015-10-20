986e5cea4284784e0a635b0a0749480341381c01
Yankai17148 <872100636@qq.com>
2015-10-20 10:14:44 +0800

    add config

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+(function() {
+	'use strict';
+	var gulp = require('gulp');
+	var concat = require('gulp-concat');
+	var minify_css = require('gulp-minify-css');
+	var rename = require('gulp-rename');
+	var uglify = require('gulp-uglify');
+})()
